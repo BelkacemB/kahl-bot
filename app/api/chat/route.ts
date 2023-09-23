4139b4edb9c7361919a09078aa73a6f8d64bd479
Belkacem Berchiche <b_berchiche@esi.dz>
2023-09-23 18:11:10 +0200

    fix: limit tokens, rekindle model

--- app/api/chat/route.ts
+++ app/api/chat/route.ts
@@ -11,25 +11,29 @@ const openai = new OpenAIApi(configuration)
 
 const systemMessage = {
   role: 'system',
-  content:
-    'You are an Algerian bureaucrat, doing your best to frustrate the user. When he makes a request, come up with absurd document requirements or outlandish excuses, but make them sound formal. Only speak in Arabic.'
-}
+  content: `You are an Algerian bureaucrat, doing your best to frustrate the user.
+            When the user asks for a favor or for an information, you must respond with an outlandish reason why you cannot do it (e.g "Network is down", "Come back tomorrow", "You need to fill out form 27B/6"), or discourage the user by giving them a short list of absurd requirements.
+            When he asks for opinion, you must respond with a non-committal answer, a vague answer or a pro-government answer.
+            You must sound formal and only speak in Arabic.`
+};
 
 export async function POST(req: Request) {
   const json = await req.json()
-  const { messages, previewToken } = json
+  let { messages, previewToken } = json
 
   if (previewToken) {
     configuration.apiKey = previewToken
   }
 
+  messages = messages.slice(-3)
   messages.unshift(systemMessage)
 
   const res = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages,
     temperature: 0.7,
-    stream: true
+    stream: true,
+    max_tokens: 500
   })
 
   const stream = OpenAIStream(res)
